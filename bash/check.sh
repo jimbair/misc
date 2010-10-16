bc19c7cb60834ff3e4e0d24bc80201d66100aa95
James Bair <james.d.bair@gmail.com>
2010-10-16 12:28:34 -0500

    Switching from tabs to spaces.

--- bash/check.sh
+++ bash/check.sh
@@ -35,75 +35,75 @@ txtrst='\e[0m'    # Text Reset
 # Exit if given control+c
 trap leaveNow 2
 leaveNow() {
-	echo 'Caught SIGINT. Exiting.'
-	exit 1
+    echo 'Caught SIGINT. Exiting.'
+    exit 1
 }
 
 # Validate IPs in function. Credits go here:
 # http://www.unix.com/shell-programming-scripting/36144-ip-address-validation-function.html
 # Fixed return @ end, cleaned up a bit, removed null check for more precise error responses.
 verify_ip() {
-	ERROR=0
-	# Backup the IFS and change
-	oldIFS=$IFS
-	IFS=.
-	# Won't lie, not 100% sure what the following 2 lines do
-	set -f
-	set -- $1
-	# Make sure we have 4 octets
-	if [ $# -eq 4 ]; then
-		for seg; do
-			case $seg in
-				# Checks for numeric chars
-				*[!0-9]*)
-				ERROR=1
-				break
-				;;
+    ERROR=0
+    # Backup the IFS and change
+    oldIFS=$IFS
+    IFS=.
+    # Won't lie, not 100% sure what the following 2 lines do
+    set -f
+    set -- $1
+    # Make sure we have 4 octets
+    if [ $# -eq 4 ]; then
+        for seg; do
+            case $seg in
+                # Checks for numeric chars
+                *[!0-9]*)
+                ERROR=1
+                break
+                ;;
 
-				# Check for numbers >255
-				*) 
-				if [ $seg -gt 255 ]; then
-					ERROR=2
-				fi
-				;;
-			esac
-		done
-	else
-		# Has more or less than 4 IP segments
-		ERROR=3
-	fi
-	# Revert to the original IRS
-	IFS=$oldIFS
-	set +f
-	# Return our status
-	return $ERROR
+                # Check for numbers >255
+                *)
+                if [ $seg -gt 255 ]; then
+                    ERROR=2
+                fi
+                ;;
+            esac
+        done
+    else
+        # Has more or less than 4 IP segments
+        ERROR=3
+    fi
+    # Revert to the original IRS
+    IFS=$oldIFS
+    set +f
+    # Return our status
+    return $ERROR
 }
 
 # Input Validation
 if [ $# -ne 1 ]; then
-	echo "$script - Script to scan commonly used TCP ports." >&2
-	echo "usage: $script host" >&2
-	exit 1
+    echo "$script - Script to scan commonly used TCP ports." >&2
+    echo "usage: $script host" >&2
+    exit 1
 fi
 
 verify_ip "$1"
 
 # 0 means we got a valid IP
 if [ $? -eq 0 ]; then
-	echo -e "\nValid IP found! Proceeding with port check."
+    echo -e "\nValid IP found! Proceeding with port check."
 # 1 means non-numeric OR empty. Run host against it.
 elif [ $? -eq 1 ]; then
-	echo -e "\nChecking if hostname given is a valid hostname."
-	host $1 > /dev/null 2>&1
-	if [ $? -ne 0 ]; then
-		echo 'ERROR: Invalid IP/Host given. Exiting.' >&2
-		exit 1
-	else
-		echo 'Valid hostname found! Proceeding with port check.'
-	fi
+    echo -e "\nChecking if hostname given is a valid hostname."
+    host $1 > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo 'ERROR: Invalid IP/Host given. Exiting.' >&2
+        exit 1
+    else
+        echo 'Valid hostname found! Proceeding with port check.'
+    fi
 else
-	echo -e "\nInvalid IP address given! Exiting." >&2
-	exit 1
+    echo -e "\nInvalid IP address given! Exiting." >&2
+    exit 1
 fi
 
 echo -e "\nPassed input validation checks. Checking ports.\n"
@@ -129,14 +129,14 @@ done
 
 ping -c 1 -w ${timeout} $1 &>/dev/null
 if [ $? -eq 0 ]; then
-	result="${txtgrn}Online"
+    result="${txtgrn}Online"
 else
-	result="${txtred}Offline"
+    result="${txtred}Offline"
 fi
-echo -e "\t${result}${txtrst}"
+echo -e "\t\t${result}${txtrst}"
 
 for port in $ports; do
-	echo -en "  Checking ${1}:${port}"
+    echo -en "  Checking ${1}:${port}"
     # Pad spaces
     ourDiff="$((${len}-${#port}))"
     if [ "${ourDiff}" -gt 0 ]; then
@@ -144,13 +144,13 @@ for port in $ports; do
             echo -n ' '
         done
     fi
-	nc -z -w ${timeout} $1 $port > /dev/null 2>&1
-	if [ $? -eq 0 ]; then
-		result="${txtgrn}Open"
-	else
-		result="${txtred}Closed"
-	fi
-	echo -e "\t${result}${txtrst}"
+    nc -z -w ${timeout} $1 $port > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        result="${txtgrn}Open"
+    else
+        result="${txtred}Closed"
+    fi
+    echo -e "\t\t${result}${txtrst}"
 done
 
 # All done!
