0e6d5c0c6786456bb25582201422f607b3f497d1
James Bair <james.d.bair@gmail.com>
2011-04-03 16:41:21 -0500

    One less grep, remove duplicate URLs now as well.

--- bash/ios-mirror.sh
+++ bash/ios-mirror.sh
@@ -4,8 +4,7 @@
 mirrorList='http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version'
 
 # Find each URL to the IPSW files.
-for url in $(curl --silent "${mirrorList}" | grep 'Restore' | grep '.ipsw' | grep -v 'protected://' | cut -d '>' -f 2- | cut -d '<' -f -1); do
-
+for url in $(curl --silent "${mirrorList}" | grep 'http://' | grep 'Restore.ipsw' | cut -d '>' -f 2 | cut -d '<' -f 1 | sort -u); do
     # Find the filename using awk
     filename="$(echo "${url}" | awk -F '/' '{print $NF}')"
     # If we have it, skip.
