f0dcf2d5bb797dd14c1d5a50fb5b3020bf9aa6e4
James Bair <james.d.bair@gmail.com>
2011-04-03 16:53:21 -0500

    Now also uses the iPhone Wiki's links to find older software releases.

--- bash/ios-mirror.sh
+++ bash/ios-mirror.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Simple script to mirror all iOS IPSW files locally from Apple's servers.
+# Uses both the iPhone Wiki as well as Apple's list of restore files.
 
-mirrorList='http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version'
+appleList='http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version'
+ipwList='http://theiphonewiki.com/wiki/index.php?title=Firmware'
 
-# Find each URL to the IPSW files.
-for url in $(curl --silent "${mirrorList}" | grep 'http://' | grep 'Restore.ipsw' | cut -d '>' -f 2 | cut -d '<' -f 1 | sort -u); do
+# Check URL and fetch if we don't have the file.
+# Expects a URL to be passed as $1
+checkAndFetch() {
     # Find the filename using awk
-    filename="$(echo "${url}" | awk -F '/' '{print $NF}')"
+    filename="$(echo "$1" | awk -F '/' '{print $NF}')"
     # If we have it, skip.
     if [ -s "${filename}" ]; then
         echo "Skipping $filename - already exists."
@@ -14,7 +17,18 @@ for url in $(curl --silent "${mirrorList}" | grep 'http://' | grep 'Restore.ipsw
     else
         wget $url && shasum $filename > $filename.sha1sum
     fi
+}
 
+# Find each URL to the IPSW files on TheiPhoneWiki.
+echo 'Beginning sync from the iPhone Wiki List:'
+for url in $(curl --silent "${ipwList}"  | grep 'Restore.ipsw' | cut -d '"' -f 2); do
+    checkAndFetch "${url}"
+done
+
+# Find each URL to the IPSW files on Apple's list.
+echo "Beginnings sync from Apple's list:"
+for url in $(curl --silent "${appleList}" | grep 'Restore.ipsw' | grep 'http://' | cut -d '>' -f 2 | cut -d '<' -f 1 | sort -u); do
+    checkAndFetch "${url}"
 done
 
 exit 0
