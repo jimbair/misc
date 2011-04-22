57d2d6729d91d8cdacd7c4db892e63debabacce2
James Bair <james.d.bair@gmail.com>
2011-04-22 17:54:30 -0500

    Cleaned up the code a bit.

--- python/dev_controller.py
+++ python/dev_controller.py
@@ -2,6 +2,7 @@
 # List what controllers your /dev/sdX or /dev/hdX devices are
 # attached to in Linux operating systems.
 # Doesn't support anything cool like SAN yet. Just local storage.
+# Only tested on Debian based systems currently (Ubuntu).
 
 import commands
 import glob
@@ -10,8 +11,10 @@ import sys
 
 def findDeviceController(device):
     """
-    Takes a given device and
+    Takes a given block device and returns the controller it's attached to.
+    Depends on lspci being installed on the system.
     """
+
     devLink = '/sys/block/' + device.split('/')[-1]
 
     if not os.path.islink(devLink):
@@ -19,26 +22,34 @@ def findDeviceController(device):
 
     # Have fun looking at this one, lol
     rawInfo = os.path.realpath(devLink)
-    old = None
+
+    # Find the last controller. The last controller is the last entry
+    # found before the host[0-9] entry, split by slashes.
+    last = None
     for item in rawInfo.split('/'):
-        if old is None:
-            old = item
+        # Set our value first.
+        if last is None:
+            last = item
             continue
 
+        # If we are on the host, our last item was the winner.
         if 'host' in item:
             break
 
-        old = item
+        # If we are here, we haven't hit the host yet.
+        last = item
 
+    # Essentially strip off the first 0's of our string.
     pciCode = ':'.join(old.split(':')[1:])
 
-    # Now find the matching device to the PCI code
+    # Find our devices/PCI codes
     status, out = commands.getstatusoutput('lspci')
     if status != 0:
         msg = "Error: Unable to execute command 'lspci'\n"
         sys.stderr.write(msg)
         sys.exit(1)
 
+    # Now find the matching device to the PCI code
     for line in out.split('\n'):
         if pciCode in line:
             return line.split(':')[2].strip()
@@ -47,24 +58,31 @@ def findDeviceController(device):
     return None
 
 def main():
+    """
+    Our main function for dev_controller.py
+    """
 
-    devPrefix = '/dev/'
-
+    # Only supported on Linux
     if sys.platform != 'linux2':
         msg = 'Error: This platform is unsupported.\n'
         msg += 'Supported platforms: Linux\n'
         sys.stderr.write(msg)
         sys.exit(1)
 
-    devices = glob.glob(devPrefix + '[s|h]d[a-z]')
+    # Find our block devices
+    devices = glob.glob('/dev/[s|h]d[a-z]')
 
+    # Make sure we found something.
     if devices == []:
         msg = 'Error: No block devices found.\n'
         sys.stderr.write(msg)
         sys.exit(1)
 
+    # Sort our devices alphabetically
     devices.sort()
 
+    # Print out our results. Should handle findDeviceController()
+    # errors better since now it prints errors in the results.
     msg = "The following devices and their controllers were found:\n\n"
     sys.stdout.write(msg)
 
@@ -75,5 +93,6 @@ def main():
 
     sys.exit(0)
 
+# Main
 if __name__ == '__main__':
     main()
