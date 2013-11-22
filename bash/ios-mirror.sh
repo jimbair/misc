dd9c8ebd00ac22f367dc01cb60a50a0cb3ca8cc0
James Bair <tsuehpsyde@gmail.com>
2013-11-22 12:47:54 -0600

    Added checksum comparison to the iPhone wiki

--- bash/ios-mirror.sh
+++ bash/ios-mirror.sh
@@ -4,6 +4,10 @@
 
 appleList='http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version'
 ipswList='http://theiphonewiki.com/wiki/Firmware'
+tmpFile='/tmp/ipw.html'
+
+export ipswList
+export tmpFile
 
 # Check URL and fetch if we don't have the file.
 # Expects a URL to be passed as $1
@@ -11,9 +15,31 @@ checkAndFetch() {
     # Find the filename using awk
     filename="$(echo "$1" | awk -F '/' '{print $NF}')"
     checksum="${filename}.sha1sum"
-    # If we have it, skip.
+    # If we have it, validate against the sha1sum on the iPhone wiki.
     if [ -s "${filename}" -a -s "${checksum}" ]; then
-        echo "Skipping ${filename}."
+        # Cache so we can grep the file each time, instead of doing a curl each time.
+        if [ ! -s "${tmpFile}" ]; then
+            echo -n "Caching iPhone wiki page..."
+            curl "${ipswList}" > "${tmpFile}" 2>/dev/null 
+            echo 'done.'
+        fi
+        # Pull and compare checksums.
+        echo -n "Validating ${filename}..."
+        wikiSum=$(grep -A 2 "${filename}" "${tmpFile}" | tail -1 | cut -d '>' -f 3 | cut -d '<' -f 1)
+        ec=$?
+        if [ -z "${wikiSum}" -o ${ec} -ne 0 ]; then
+            echo 'failed.'
+            echo "Unable to fetch sha1sum from The iPhone Wiki. Exiting."
+            exit 1
+        fi
+        ourSum=$(awk '{print $1}' ${checksum})
+        # Would like to destroy and re-fetch. For now, leave it manual.
+        if [ "${wikiSum}" != "${ourSum}" ]; then
+            echo 'failed.'
+            echo "WARNING: Checksums do not match! Exiting."
+            exit 1
+        fi
+        echo 'done.'
     # Look for missing checksum
     elif [ -s "${filename}" -a ! -s "${checksum}" ]; then
         echo "Checksum file ${checksum} missing. Generating..."
@@ -54,4 +80,5 @@ for url in $(curl --silent "${appleList}" | grep 'Restore.ipsw' | grep 'http://'
     checkAndFetch "${url}"
 done
 
+rm -f "${tmpFile}"
 exit 0
