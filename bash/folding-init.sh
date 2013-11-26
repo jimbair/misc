c7eeb3f1d8661534c468dcaed0d7a7093f29ce4f
James Bair <tsuehpsyde@gmail.com>
2013-11-26 09:27:06 -0600

    Adding this into git so I have it somewhere. =)

---
+++ bash/folding-init.sh
@@ -0,0 +1,45 @@
+# Basic script to stop/start folding@home.
+# Built for my needs - expecting ~/folding/ to exist.
+# Validate inputs
+if [ $# -ne 1 ]; then
+    echo "Usage: $(basename $0) start|stop"
+    exit 1
+fi
+if [ $1 != 'start' ]; then
+    if [ $1 != 'stop' ]; then
+        if [ $1 != 'status' ]; then
+            echo "Usage: $(basename $0) start|stop|status"
+            exit 1
+    fi
+    fi
+fi
+
+# Start
+if [ $1 == 'start' ]; then
+    ps aux | grep fah6 | grep -q -v grep
+    if [ $? -eq 0 ]; then
+        echo "Folding already running."
+        exit 0
+    fi
+    cd ${HOME}/folding; ./fah6 -smp &>/dev/null &
+    echo "Folding started."
+# Stop
+elif [ $1 == 'stop' ]; then
+    ps aux | grep fah6 | grep -q -v grep
+    if [ $? -eq 1 ]; then
+        echo "Folding already stopped."
+        exit 0
+    fi
+    pkill fah6
+    echo "Folding stopped."
+# Status
+elif [ $1 == 'status' ]; then
+    ps aux | grep fah6 | grep -q -v grep
+    if [ $? -eq 0 ]; then
+        echo "Folding is running."
+        exit 0
+    else
+        echo "Folding is stopped."
+        exit 0
+    fi
+fi
