746fcd9e77fe75564cb10303c3217ad82f5c1b79
James Bair <tsuehpsyde@gmail.com>
2018-11-26 09:51:59 -0600

    Small update around variable names

--- bash/homeDNS.sh
+++ bash/homeDNS.sh
@@ -7,6 +7,6 @@ which rsdns > /dev/null 2>&1 || exit 1
 /bin/date
 IPV4=$(curl --silent ipv4.icanhazip.com)
 IPV6=$(curl --silent ipv6.icanhazip.com)
-[ -n "${IPV4}" ] && [ -z "$(host $DNS | grep $IPV4)" ] && rsdns a -i $IPV4 -n $DNS -U
-[ -n "${IPV6}" ] && [ -z "$(host $DNS | grep $IPV6)" ] && rsdns aaaa -i $IPV6 -n $DNS -U
+[ -n "${IPV4}" ] && [ -z "$(host ${DNS} | grep ${IPV4})" ] && rsdns a -i ${IPV4} -n ${DNS} -U
+[ -n "${IPV6}" ] && [ -z "$(host ${DNS} | grep ${IPV6})" ] && rsdns aaaa -i ${IPV6} -n ${DNS} -U
 exit 0
