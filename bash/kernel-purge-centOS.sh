59a38f3ebbedd2ab3cdd910c38fb23050dc97036
Jim Bair <jim.bair@rackspace.com>
2019-02-20 13:30:44 -0600

    Consolidate the yum removals

--- bash/kernel-purge-centOS.sh
+++ bash/kernel-purge-centOS.sh
@@ -2,10 +2,10 @@
 # A quick one-liner to yank old kernels in CentOS 7
 # yum-utils could do this as well, but I couldn't use it sadly.
 # 
-# One-liner: # for kernel in $(rpm -qa | grep kernel-3); do [[ "${kernel}" == "kernel-$(uname -r)" ]] && continue; yum remove -y ${kernel}; yum remove -y $(sed s/kernel/kernel-devel/ <<< ${kernel}); done
+# One-liner:
+# for kernel in $(rpm -qa | grep kernel-3); do [[ "${kernel}" == "kernel-$(uname -r)" ]] && continue; yum remove -y ${kernel} $(sed s/kernel/kernel-devel/ <<< ${kernel}); done
 
  for kernel in $(rpm -qa | grep kernel-3); do
     [[ "${kernel}" == "kernel-$(uname -r)" ]] && continue
-    yum remove -y ${kernel}
-    yum remove -y $(sed s/kernel/kernel-devel/ <<< ${kernel})
+    yum remove -y ${kernel} $(sed s/kernel/kernel-devel/ <<< ${kernel})
 done
