b9ac670d1040159a7c70eec9a5fa67488df0dcf8
Jim Bair <james.d.bair@gmail.com>
2020-05-02 07:41:52 -0500

    Fix for e1000e driver on RHEL 8.2

---
+++ bash/e1000e-fix.sh
@@ -0,0 +1,13 @@
+# Fixes e1000e continuously resetting
+# Lock to a kernel version so test new updates
+# 
+# v.01 - May 2nd, 2020
+# Jim Bair
+kernel_release='4.18.0-193.el8.x86_64'
+gateway_dev=$(ip r | grep ^default | cut -d ' ' -f 5)
+
+# Make sure we're root, we found our gw device, and disable the bad bits
+# https://serverfault.com/questions/616485/e1000e-reset-adapter-unexpectedly-detected-hardware-unit-hang
+[[ "$UID" == 0 ]] || exit 1
+[[ -n "${gateway_dev}" ]] || exit 2
+[[ $(uname -r) == "${kernel_release}" ]] && echo ethtool -K ${gateway_dev} gso off gro off tso off
