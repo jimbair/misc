355715e6d4246512ec5521d8d7f36f84afa72f8d
Jim Bair <james.d.bair@gmail.com>
2020-10-01 09:11:12 -0500

    Update debian-torrents-fetch.sh

--- bash/debian-torrents-fetch.sh
+++ bash/debian-torrents-fetch.sh
@@ -2,11 +2,11 @@
 # A script to fetch all debian torrents, since they make this
 # ridiculously difficult to do for some reason. This script
 # is 100% a hack - it fetches all torrents twice, and lets
-# the find sort it out. Be sure to run this in a folder. =) 
+# find sort it out. The torrents are dropped into the torrents folder.
 #
 # I use this to keep a fresh set of torrents seeding on tsue.net
 #
-# v1.0
+# v1.1
 
 
 # Grab them all
