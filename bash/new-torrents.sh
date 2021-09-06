4b80393f7334c50d01edc57fa865900e7a268ad0
Jim Bair <james.d.bair@gmail.com>
2021-09-06 10:10:18 -0500

    Move ubuntu check to torrent tracker

--- bash/new-torrents.sh
+++ bash/new-torrents.sh
@@ -12,9 +12,9 @@ ROCKY='8.4'
 ALMA="${ROCKY}"
 # Don't forget about 7
 CENTOS7='2009'
-# Ubuntu makes this hard so we scrape the entire releases page and sha1sum it
-# Generated Sep 4th 2021
-UBUNTU='71a561d8b1c98f8556bcfbd9f6c80e4fc1799598'
+# Ubuntu makes this hard so we scrape the torrent tracker and sha1sum it
+# Generated Sep 6th 2021
+UBUNTU='5e304d75e6c39338572f0dea4aa6181fa7804603'
 
 # Run the checks
 curl -s https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ | grep -q "${DEBIAN}" || exit 1
@@ -23,7 +23,7 @@ curl -s https://mirror.rackspace.com/centos/8/isos/x86_64/ | grep -q "${CENTOS}"
 curl -s https://download.rockylinux.org/pub/rocky/8/isos/x86_64/ | grep -q "${ROCKY}" || exit 1
 curl -s https://mirror.rackspace.com/almalinux/8/isos/x86_64/ | grep -q "${ALMA}" || exit 1
 curl -s https://mirror.rackspace.com/centos/7/isos/x86_64/ | grep -q "${CENTOS7}" || exit 1
-curl -s https://wiki.ubuntu.com/Releases | grep e5e5e5 | sha1sum | grep -q "${UBUNTU}" || exit 1
+curl -s https://torrent.ubuntu.com/tracker_index | grep iso | cut -d '>' -f 8 | sha1sum | grep -q "${UBUNTU}" || exit 1
 
 # We made it
 exit 0
