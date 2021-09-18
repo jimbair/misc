ea042c1bf3c7b316f4e2ef02e0aade54493069fb
Jim Bair <james.d.bair@gmail.com>
2021-09-18 15:41:12 -0500

    Unique exit codes

--- bash/new-torrents.sh
+++ bash/new-torrents.sh
@@ -18,12 +18,12 @@ UBUNTU='5e304d75e6c39338572f0dea4aa6181fa7804603'
 
 # Run the checks
 curl -s https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ | grep -q "${DEBIAN}" || exit 1
-curl -s https://mirror.rackspace.com/fedora/releases/ | grep -q "${FEDORA}" && exit 1
-curl -s https://mirror.rackspace.com/centos/8/isos/x86_64/ | grep -q "${CENTOS}" || exit 1
-curl -s https://download.rockylinux.org/pub/rocky/8/isos/x86_64/ | grep -q "${ROCKY}" || exit 1
-curl -s https://mirror.rackspace.com/almalinux/8/isos/x86_64/ | grep -q "${ALMA}" || exit 1
-curl -s https://mirror.rackspace.com/centos/7/isos/x86_64/ | grep -q "${CENTOS7}" || exit 1
-curl -s https://torrent.ubuntu.com/tracker_index | grep iso | cut -d '>' -f 8 | sha1sum | grep -q "${UBUNTU}" || exit 1
+curl -s https://mirror.rackspace.com/fedora/releases/ | grep -q "${FEDORA}" && exit 2
+curl -s https://mirror.rackspace.com/centos/8/isos/x86_64/ | grep -q "${CENTOS}" || exit 3
+curl -s https://download.rockylinux.org/pub/rocky/8/isos/x86_64/ | grep -q "${ROCKY}" || exit 4
+curl -s https://mirror.rackspace.com/almalinux/8/isos/x86_64/ | grep -q "${ALMA}" || exit 5
+curl -s https://mirror.rackspace.com/centos/7/isos/x86_64/ | grep -q "${CENTOS7}" || exit 6
+curl -s https://torrent.ubuntu.com/tracker_index | grep iso | cut -d '>' -f 8 | sha1sum | grep -q "${UBUNTU}" || exit 7
 
 # We made it
 exit 0
