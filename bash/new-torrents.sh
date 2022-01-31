72888806ba87ac98d64a9cb2860517069785ddb1
Jim Bair <james.d.bair@gmail.com>
2022-01-31 08:56:22 -0600

    Remove legacy CentOS 8 Check

--- bash/new-torrents.sh
+++ bash/new-torrents.sh
@@ -5,8 +5,6 @@
 DEBIAN='11.1.0'
 # Check for next release to show up as there is no current directory to look for
 FEDORA='36'
-# Checks for release found in current directory
-CENTOS='8.5.2111'
 # Alma only use the first two numbers
 ALMA='8.5'
 # Don't forget about 7
@@ -17,19 +15,18 @@ UBUNTU='/tmp/ubuntu-torrents.txt'
 # Run the checks
 curl -s https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ | grep -q "${DEBIAN}" || exit 1
 curl -s https://mirror.rackspace.com/fedora/releases/ | grep -q "${FEDORA}" && exit 2
-curl -s https://mirror.rackspace.com/centos/8/isos/x86_64/ | grep -q "${CENTOS}" || exit 3
-curl -s https://mirror.rackspace.com/almalinux/8/isos/x86_64/ | grep -q "${ALMA}" || exit 4
-curl -s https://mirror.rackspace.com/centos/7/isos/x86_64/ | grep -q "${CENTOS7}" || exit 5
+curl -s https://mirror.rackspace.com/almalinux/8/isos/x86_64/ | grep -q "${ALMA}" || exit 3
+curl -s https://mirror.rackspace.com/centos/7/isos/x86_64/ | grep -q "${CENTOS7}" || exit 4
 
 # Create temp file if missing
 if [ ! -s "${UBUNTU}" ]; then
-  curl -s https://torrent.ubuntu.com/tracker_index | grep iso | cut -d '>' -f 8 > ${UBUNTU} || exit 6
+  curl -s https://torrent.ubuntu.com/tracker_index | grep iso | cut -d '>' -f 8 > ${UBUNTU} || exit 5
   exit 0
 fi
 
 # See what has changed in Ubuntu and clean-up if no changes
 curl -s https://torrent.ubuntu.com/tracker_index | grep iso | cut -d '>' -f 8 > ${UBUNTU}.new
-diff -q ${UBUNTU} ${UBUNTU}.new > /dev/null || exit 7
+diff -q ${UBUNTU} ${UBUNTU}.new > /dev/null || exit 6
 rm -f ${UBUNTU}.new
 
 # We made it
