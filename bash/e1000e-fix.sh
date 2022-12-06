52518d955e42eaa274de89804958f99b7e919675
Jim Bair <james.d.bair@gmail.com>
2022-12-06 12:26:29 -0600

    Remove kernel version check
    This has persisted for RHEL 8.2 through 9.1, so the kernel-specific logic serves no purpose any longer.

--- bash/e1000e-fix.sh
+++ bash/e1000e-fix.sh
@@ -1,18 +1,17 @@
 # Fixes e1000e continuously resetting
 # Lock to a kernel version to test new updates
 # 
-# v.02 - May 2nd, 2020
+# v.03
 # Jim Bair
 
 # root's crontab only has /bin and /usr/bin in RHEL 8.2.0
 PATH='/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin'
 
-# Blacklisting current kernel known to have the bug
-kernel_release='4.18.0-193.1.2.el8_2.x86_64'
+# Pull our gateway device dynamically
 gateway_dev=$(ip r | grep ^default | cut -d ' ' -f 5)
 
 # Make sure we're root, we found our gw device, and disable the bad bits
 # https://serverfault.com/questions/616485/e1000e-reset-adapter-unexpectedly-detected-hardware-unit-hang
 [[ "$UID" == 0 ]] || exit 1
 [[ -n "${gateway_dev}" ]] || exit 2
-[[ $(uname -r) == "${kernel_release}" ]] && ethtool -K ${gateway_dev} gso off gro off tso off
+ethtool -K ${gateway_dev} gso off gro off tso off
