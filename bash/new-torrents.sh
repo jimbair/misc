a16cbb1c51503dadd8bea65b9721cdcfb80d397d
Jim Bair <james.d.bair@gmail.com>
2023-05-04 13:56:25 -0500

    Update new-torrents.sh

--- bash/new-torrents.sh
+++ bash/new-torrents.sh
@@ -2,7 +2,7 @@
 # Check for updates to torrents for our mirror
 
 # Checks for release found in current directory
-DEBIAN='11.6.0'
+DEBIAN='11.7.0'
 # Check for next release to show up as there is no current directory to look for
 FEDORA='39'
 # Alma only uses the first two numbers
