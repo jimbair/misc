8c0964d279d0ff3705a17ff7eaabf6e68b6bfccc
Jim Bair <james.d.bair@gmail.com>
2024-12-15 08:37:27 -0600

    Migrate Alma Check to 9
    Time to move the check to Alma 9 as I don't use alma 8 anymore

--- bash/new-torrents.sh
+++ bash/new-torrents.sh
@@ -6,7 +6,7 @@ DEBIAN='12.8.0'
 # Check for next release to show up as there is no current directory to look for
 FEDORA='42'
 # Alma only uses the first two
-ALMA='8.10'
+ALMA='9.5'
 # Ubuntu makes this hard so we scrape the torrent tracker and diff it
 UBUNTU='/tmp/ubuntu-torrents.txt'
 
@@ -16,7 +16,7 @@ UPDATES=''
 # Run the checks
 curl -s https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ | grep -q "${DEBIAN}" || UPDATES='Debian'
 curl -s https://mirror.rackspace.com/fedora/releases/ | grep -q "${FEDORA}" && UPDATES="${UPDATES} Fedora"
-curl -s https://mirror.rackspace.com/almalinux/8/isos/x86_64/ | grep -q "${ALMA}" || UPDATES="${UPDATES} Alma 8"
+curl -s https://mirror.rackspace.com/almalinux/9/isos/x86_64/ | grep -q "${ALMA}" || UPDATES="${UPDATES} Alma 9"
 
 # Create temp file if missing
 if [ ! -s "${UBUNTU}" ]; then
