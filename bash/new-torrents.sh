6d7fe0c02ffecab21220b39a090eb2289d7b1e93
Jim Bair <james.d.bair@gmail.com>
2025-11-03 15:31:06 -0600

    Update new-torrents.sh

--- bash/new-torrents.sh
+++ bash/new-torrents.sh
@@ -4,7 +4,7 @@
 # Checks for release found in current directory
 DEBIAN='13.0.0'
 # Check for next release to show up as there is no current directory to look for
-FEDORA='43'
+FEDORA='44'
 # Alma only uses the first two
 ALMA='9.5'
 # Ubuntu makes this hard so we scrape the torrent tracker and diff it
