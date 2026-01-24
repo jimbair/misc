4b94a8bddd779bf828dae6521adbf5eff2300ce3
Jim Bair <james.d.bair@gmail.com>
2026-01-24 09:11:47 -0600

    Check all proxmox versions
    This could be done more gooder but for my purposes this is fine enough

--- bash/new-torrents.sh
+++ bash/new-torrents.sh
@@ -8,8 +8,11 @@ FEDORA='44'
 # Alma only uses the first two
 ALMA9='9.7'
 ALMA10='10.1'
-# Proxmox latest
-PROXMOX='9.1-1'
+# Proxmox
+PROXMOX6='6.4-1'
+PROXMOX7='7.4-1'
+PROXMOX8='8.4-1'
+PROXMOX9='9.1-1'
 # Ubuntu makes this hard so we scrape the torrent tracker and diff it
 UBUNTU='/tmp/ubuntu-torrents.txt'
 
@@ -19,12 +22,19 @@ UPDATES=''
 # cURL Options
 COPTS='-s -m 5'
 
-# Run the checks
+# Run the simple checks
 curl ${COPTS} https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/ | grep -q "${DEBIAN}" || UPDATES='Debian'
 curl ${COPTS} https://mirror.rackspace.com/fedora/releases/ | grep -q "${FEDORA}" && UPDATES="${UPDATES} Fedora"
 curl ${COPTS} https://mirror.rackspace.com/almalinux/9/isos/x86_64/ | grep -q "${ALMA9}" || UPDATES="${UPDATES} Alma 9"
 curl ${COPTS} https://mirror.rackspace.com/almalinux/10/isos/x86_64/ | grep -q "${ALMA10}" || UPDATES="${UPDATES} Alma 10"
-curl ${COPTS} https://www.proxmox.com/en/downloads/proxmox-virtual-environment/iso | grep -q "${PROXMOX}" || UPDATES="${UPDATES} Proxmox 9"
+
+# Let's be nice to their server
+curl ${COPTS} https://www.proxmox.com/en/downloads/proxmox-virtual-environment/iso > /tmp/pm-cache
+grep -q ${PROXMOX6} /tmp/pm-cache || UPDATES="${UPDATES} Proxmox 6"
+grep -q ${PROXMOX7} /tmp/pm-cache || UPDATES="${UPDATES} Proxmox 7"
+grep -q ${PROXMOX8} /tmp/pm-cache || UPDATES="${UPDATES} Proxmox 8"
+grep -q ${PROXMOX9} /tmp/pm-cache || UPDATES="${UPDATES} Proxmox 9"
+rm -f /tmp/pm-cache
 
 # Create temp file if missing
 if [ ! -s "${UBUNTU}" ]; then
