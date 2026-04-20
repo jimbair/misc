5d9be624bccbefb14ca8fa83b5a1d8a7e51756cd
Jim Bair <james.d.bair@gmail.com>
2026-04-19 21:17:56 -0500

    Update max-clock.sh

--- bash/max-clock.sh
+++ bash/max-clock.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # A simple script to find the max speed of your boosting CPU
 MAX=$(grep MHz /proc/cpuinfo | sort -n | tail -1 | cut -d ' ' -f 3 | cut -d . -f 1)
+
+# Doesn't work on ARM VMs! Good to validate and abort if missing
+if [[ -z "${MAX}" ]]; then
+  echo "ERROR: Unable to find processor speed. Exiting." >&2
+  exit 1
+fi
+
+# Do the thang
 while true; do
   CURRENT=$(grep MHz /proc/cpuinfo | sort -n | tail -1 | cut -d ' ' -f 3 | cut -d . -f 1)
   [[ ${CURRENT} -gt ${MAX} ]] && MAX=${CURRENT}
