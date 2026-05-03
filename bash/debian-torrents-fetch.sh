399db79d863e6f65a6ca76d532496fb64177f920
Jim Bair <james.d.bair@gmail.com>
2026-05-03 07:52:49 -0500

    Move to rsync
    In an attempt to speed up this script, I have moved from the hacky recursive wget to a more proper rsync fetch

--- bash/debian-torrents-fetch.sh
+++ bash/debian-torrents-fetch.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-# A script to fetch all debian torrents, since they make this
-# ridiculously difficult to do for some reason. This script
-# is 100% a hack - it fetches all torrents twice, and lets
-# find sort it out. The torrents are dropped into the torrents folder.
-#
-# I use this to keep a fresh set of torrents seeding on tsue.net
-#
-# v1.1
+# A script to fetch all of the current debian torrents
+# Leverages rsync for speed; this is only run when a new release
+# is dropped and I need to update mirror.tsue.net's torrents
 
+# Create two directories, one for the rsync target and one to easily grab all torrents
+mkdir -p torrents_tree torrents
 
-# Grab them all
-wget -r -A.torrent https://cdimage.debian.org/debian-cd/current/ || exit 1
+# Sync the tree (only downloads .torrent files)
+rsync -am --include='*/' --include='*.torrent' --exclude='*' rsync://cdimage.debian.org/debian-cd/current/ torrents_tree/ || exit 1
 
-# Move all the torrents into a folder
-mkdir torrents
-find cdimage.debian.org -type f -name '*.torrent' -exec mv '{}' torrents/ \; || exit 2
-rm -fr cdimage.debian.org
+# Move torrent files into a single directory
+find torrents_tree -type f -name '*.torrent' -exec mv '{}' torrents/ \; || exit 2
+
+# Remove tree (which should now be empty)
+rm -r torrents_tree || exit 3
