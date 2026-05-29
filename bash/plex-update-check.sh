fad5942f2cbeee64c14c2c164a0240ae833119f8
Jim Bair <james.d.bair@gmail.com>
2026-05-29 14:50:56 -0500

    Style updates

--- bash/plex-update-check.sh
+++ bash/plex-update-check.sh
@@ -5,7 +5,7 @@
 # Can also be extracted locally but the api validates that our service is up
 INSTALLED=$(curl -s "http://localhost:32400/identity" | grep -oP \
   '<MediaContainer[^>]+version="\K[^"]+')
-if [ -z "$INSTALLED" ]; then
+if [[ -z "${INSTALLED}" ]]; then
   echo "Could not read installed Plex version from local API."
   exit 1
 fi
@@ -13,18 +13,18 @@ fi
 # No auth required which is nice
 API_JSON=$(curl -s "https://plex.tv/api/downloads/5.json")
 
-LATEST=$(echo "$API_JSON" | jq -r \
+LATEST=$(echo "${API_JSON}" | jq -r \
   ".nas[\"Synology (DSM 7)\"].version // .nas.Synology.version" 2>/dev/null)
-if [ -z "$LATEST" ]; then
+if [[ -z "${LATEST}" ]]; then
   echo "Could not read latest version from Plex API."
   exit 1
 fi
 
 # Alert if needed
-if [ "$INSTALLED" != "$LATEST" ]; then
+if [[ "${INSTALLED}" != "${LATEST}" ]]; then
   echo "Update available"
-  echo "Installed: $INSTALLED"
-  echo "Latest:    $LATEST"
+  echo "Installed: ${INSTALLED}"
+  echo "Latest:    ${LATEST}"
   exit 1
 fi
 
